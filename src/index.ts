d6845f2eaf56ce7f41bbe3ebf9343c15f3c014f4
Rayhan Arkan <rayhanarkan30@gmail.com>
2024-09-14 15:45:57 +0700

    add login register route

--- src/index.ts
+++ src/index.ts
@@ -105,7 +105,7 @@ app.post(`/signin`, async (req, res) => {
       .setExpirationTime("30m")
       .sign(secret);
 
-    res.json({
+    res.status(200).json({
       status: "success",
       code: 200,
       data: { jwt },
@@ -123,7 +123,85 @@ app.post(`/signin`, async (req, res) => {
 app.post(`/signup`, async (req, res) => {
   try {
     const { email, phone, password } = req.body;
-  } catch (error: any) {}
+
+    const errors: {
+      emailError?: string;
+      phoneError?: string;
+      passwordError?: string;
+    } = {};
+
+    if (!email) {
+      errors.emailError = "Email is required";
+    }
+    if (!phone) {
+      errors.phoneError = "Phone is required";
+    }
+    if (!password) {
+      errors.passwordError = "Password is required";
+    }
+    if (email && !isValidEmail(email)) {
+      errors.emailError = "Invalid email format";
+    }
+    if (phone && !isValidPhone(phone)) {
+      errors.phoneError = "Invalid phone format";
+    }
+    if (password && !isValidPassword(password)) {
+      errors.passwordError =
+        "Password must contain at least 8 characters, 1 uppercase letter, and 1 number";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).send({
+        status: "error",
+        code: 400,
+        data: { email, phone, password },
+        message: errors,
+      });
+    }
+
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { phone }],
+      },
+    });
+
+    if (existingUser){
+      return res.status(400).send({
+        status: "error",
+        code: 400,
+        data: { email, phone },
+        message: "User already exists with this email or phone",
+      });
+    }
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        phone,
+        password: await bcrypt.hash(password, 10),
+        profile: {
+          create: {
+            name: email.split("@")[0],
+            username: email.split("@")[0],
+            bio: "Halo, saya baru disini!",
+          }
+        }
+      },
+    });
+
+    res.status(201).json({
+      status: "success",
+      code: 201,
+      data: { newUser },
+      message: "Successfully signed up",
+    });
+  } catch (error: any) {
+    res.status(500).send({
+      status: "error",
+      code: 500,
+      message: "Internal server error" + error.message,
+    });
+  }
 });
 
 const server = app.listen(3000, () =>
